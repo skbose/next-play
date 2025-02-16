e9d4ad9eb53ce19363c87cab3c53f722dc5dee78
Sourav Kumar Bose <sourav.kumar.bose@gmail.com>
2025-02-16 10:25:47 +0530

    added backend handler

--- backend/app/authentication/urls.py
+++ backend/app/authentication/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import register_user, login_user, logout_view
 
 urlpatterns = [
-    path('register/', register_user, name='register'),
-    path('login/', login_user, name='login'),
-    path('logout/', logout_view, name='logout'),
+    path('backend/register/', register_user, name='register'),
+    path('backend/login/', login_user, name='login'),
+    path('backend/logout/', logout_view, name='logout'),
 ]
